b26f126802ac06088e0f4b41eacf723c7dcef5c2
OrlandoEduardo101 <orlando.eduardo101@gmail.com>
2020-02-12 06:17:33 -0300

    interface home

---
+++ lib/api.dart
@@ -0,0 +1,1 @@
+const CHAVE_YOTUBE_API = "AIzaSyDaLLM7GK4AseHf_9EQnpmDinnsrsJ2qlE";
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/biblioteca.dart';
+import 'package:youtube/telas/emAlta.dart';
+import 'package:youtube/telas/inicio.dart';
+import 'package:youtube/telas/inscricao.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,15 +10,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+  List<MaterialColor> _appbar = [Colors.blue, Colors.yellow, Colors.green,Colors.blue];
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
           color:Colors.red,
           opacity: 0.8,
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: _appbar[_indiceAtual],
           title: Image.asset(
             "images/youtube.png",
             width:98,
@@ -41,7 +57,41 @@ class _HomeState extends State<Home> {
               ),
           ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.red,
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+          print(_indiceAtual);
+          setState(() {
+            _indiceAtual = indice;
+            print(_indiceAtual);
+          });
+        },
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: _appbar[0],
+            title: Text("inicio"),
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor:  _appbar[1],
+            title: Text("em alta"),
+            icon: Icon(Icons.whatshot)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor:  _appbar[2],
+            title: Text("inscrições"),
+            icon: Icon(Icons.subscriptions)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor:  _appbar[3],
+            title: Text("Biblioteca"),
+            icon: Icon(Icons.folder)
+          ),
+        ]
+      ),
     );
   }
 }
